b887e1444afe1dbc78267b62ca6c455ecdd3cb1c
ThiagoWhispher <thiagoyeds2013@gmail.com>
2017-03-03 07:02:45 -0300

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,20 +1,10 @@
 import webapp2
 
-form_html = '''
-<form>
-<h2>Adicione uma comida</h2>
-<input type="text" name="food">
-<button>Adicionar</button>
-</form>
-'''
-
-class Handler(webapp2.RequestHandler):
-	def write(selt, *a, **kw):
-		self.response.out.write(*a, **kw)
+form=open("index.html").read()
 
 class MainHandler(Handler):
 	def get(self):
-		self.write(form_html)
+		self.write(form)
 
 app = webapp2.WSGIApplication([
 	('/',MainHandler)
