d85431a473fc715ab6c5175ad85d353d0057b5aa
ThiagoWhispher <thiagoyeds2013@gmail.com>
2017-03-03 08:48:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ class MainHandler(Handler):
 	def get(self):
 		self.write(form)
 
+class CursosHandler(Handler):
+	def get(self):
+		self.write(form)
+		
+class ProfessorHandler(Handler):
+	def get(self):
+		self.write(form)
+
 app = webapp2.WSGIApplication([
 	('/',MainHandler)
+	('/cursos',CursosHandler),
+	('/professor',ProfessorHandler)
 ], debug=True)
